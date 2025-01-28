e3488bd87c194d5585558fe5fa090c1b28337598
shrestha <shresthachakrabortykgp@gmail.com>
2025-01-28 18:26:46 +0530

    user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler";
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
+  return res.status(200).json({
     message: "ok",
   });
 });
