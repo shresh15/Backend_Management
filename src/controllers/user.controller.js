25f4a29f4eee6214a44c7111ec6bbb4b519e034e
shrestha <shresthachakrabortykgp@gmail.com>
2025-01-30 00:13:10 +0530

    All setup configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,7 @@
-import { asyncHandler } from "../utils/asyncHandler";
-const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    message: "ok",
-  });
+import { asyncHandler } from "../utils/asyncHandler.js";
+const registerUser = asyncHandler((req, res, next) => {
+  res.send("registered successfully");
+  //return new Promise()
 });
 
 export { registerUser };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
-
+import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 
 dotenv.config({
-  path: "./env",
+  path: ".env",
 });
 connectDB()
   .then(() => {
@@ -14,24 +14,24 @@ connectDB()
   .catch((err) => {
     console.log("MONGO DB connection failed !!!", err);
   });
-/*
-import express from "express";
-const app=express();
-(async ()=>{
-    try{
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-    app.on("error", (error) =>{
-        console.log("ERRR",error);
-        throw error
-    })
-    app.listen(process.env.PORT,()=>{
-        console.log(`App is listeninig on port ${process.env.PORT}`);
 
-    })
-}
-    catch(error){
-        console.error("ERROR",error)
-        throw error;
-    }
-})()
-    */
+// import express from "express";
+// const app = express();
+// (async () => {
+//   try {
+//     // await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//     // app.on("error", (error) => {
+//     //   console.log("ERRR", error);
+//     //   throw error;
+//     // });
+//     app.get("/", (req, res) => {
+//       res.send("Gethello");
+//     });
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`App is listeninig on port ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("ERROR", error);
+//     throw error;
+//   }
+// })();
