97533b1fb291366db90cd533ffa8076d7fa7652b
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-03 00:22:22 +0530

    guidelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,12 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 const registerUser = asyncHandler((req, res, next) => {
+  //get user details from frontend
+  //validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload them to cloudinary, avatar
+  // create user object- create entry in db
+
   res.send("registered successfully");
   //return new Promise()
 });
