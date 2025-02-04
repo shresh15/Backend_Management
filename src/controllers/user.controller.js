12b160e3bdcce65a5c33870ba72e94fdc87cd973
shrestha <shresthachakrabortykgp@gmail.com>
2025-02-04 23:42:22 +0530

    requesting from body

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 const registerUser = asyncHandler((req, res, next) => {
-  //get user details from frontend
-  //validation - not empty
-  // check if user already exists: username, email
-  // check for images, check for avatar
-  // upload them to cloudinary, avatar
-  // create user object- create entry in db
-
-  res.send("registered successfully");
-  //return new Promise()
+  /*1. get user details from frontend
+  2.validation - not empty
+  3.check if user already exists: username, email
+  4.check for images, check for avatar
+  5.upload them to cloudinary, avatar
+  6.create user object- create entry in db
+  7.remove password and refresh token field from response
+  8.check for user creation
+  9.return res
+  */
+  const { fullName, email, username, password } = req.body;
+  console.log("email", email);
 });
 
 export { registerUser };
